c5c1be0b43931349730dc977c1d68aebadade780
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-05-05 08:23:02 -0400

    Update CLI.rb
    basic interface setup

--- bin/CLI.rb
+++ bin/CLI.rb
@@ -2,6 +2,22 @@
 
 class CLI
     def call
-        puts "hello world"
+        puts "Welcome to Recipe Scraper!"
+        puts "Enter 'help' for a list of commands."
+        puts "Enter 'exit' to close app."
+        response = ""
+        until response == "exit"
+            response = gets.strip
+            if response == "exit"
+            elsif response == "help"
+                self.help
+            else
+                puts "Invalid command, please try again."
+            end
+        end
+    end
+    def help
+        puts "For a list of recipes, enter 'recipes'"
+        # puts rest of commands
     end
 end
